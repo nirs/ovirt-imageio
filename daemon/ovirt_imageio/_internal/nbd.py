3991549c9ccdc1f7534bd6bab47da759c201560c
Nir Soffer <nsoffer@redhat.com>
2020-06-09 20:01:50 +0300

    nbd: Add Extent.hole property
    Extent.hole exposes the NBD_STATE_HOLE bit when querying
    "base:allocation" meta context. We can use this to detect unallocated
    areas when reading from single qcow2 image.
    
    Change-Id: Ic959d6ceeff803db3724dab333d6a58c4ce258f6
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- daemon/ovirt_imageio/_internal/nbd.py
+++ daemon/ovirt_imageio/_internal/nbd.py
@@ -1358,6 +1358,10 @@ class Extent(object):
         return bool(self._flags & STATE_ZERO)
 
     @property
+    def hole(self):
+        return bool(self._flags & STATE_HOLE)
+
+    @property
     def dirty(self):
         """
         For qemu:dirty-bitmap extent, True if extents was modified and is
